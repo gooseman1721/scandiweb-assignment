8f6102c90c9e570dc36df9bf408b3f8082cd8729
Gooseman <theados123@gmail.com>
2022-11-11 23:22:12 +0100

    Added transitions to ProductCard. Quick buy button progress.

--- src/components/TopBar/ProductCard.jsx
+++ src/components/TopBar/ProductCard.jsx
@@ -4,6 +4,18 @@ import { Component } from "react";
 
 import { css } from "@emotion/react";
 
+const quickAddToCartStyle = css`
+  position: relative;
+  height: 52px;
+  width: 52px;
+  left: 303px;
+  bottom: 102px;
+  border-radius: 100%;
+  background-color: #5ece7b;
+  transition: 0.1s;
+  opacity: 0;
+`;
+
 export default class ProductCard extends Component {
   render() {
     return (
@@ -12,7 +24,15 @@ export default class ProductCard extends Component {
           height: 444px;
           width: 386px;
           padding: auto;
-          /* border: 2px dotted; */
+          transition: 0.1s;
+          &:hover {
+            box-shadow: 0px 4px 35px rgba(168, 172, 176, 0.19);
+            transition: 0.1s;
+          }
+          &:hover [data-comp="quickAddToCart"] {
+            transition: 0.1s;
+            opacity: 1;
+          }
         `}
       >
         <div
@@ -23,6 +43,7 @@ export default class ProductCard extends Component {
             justify-content: center;
             align-items: center;
             overflow: hidden;
+            margin: auto;
           `}
         >
           <img
@@ -34,9 +55,13 @@ export default class ProductCard extends Component {
             `}
           />
         </div>
-        <div css={css`
-          margin-top: 24px;
-        `}>
+        <div
+          css={css`
+            margin-top: 24px;
+            margin-left: 16px;
+            margin-right: 16px;
+          `}
+        >
           <div
             css={css`
               font-family: "Raleway";
@@ -64,6 +89,7 @@ export default class ProductCard extends Component {
             {this.props.productData.prices[0].amount}
           </div>
         </div>
+        <div css={quickAddToCartStyle} data-comp="quickAddToCart"></div>
       </div>
     );
   }
