af28e7ed25cfce0c6eed94c8547f73456e44d958
Gooseman <theados123@gmail.com>
2022-12-20 23:28:09 +0100

    Removed unused files.

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import cartReducer from "../features/product_data/cartSlice";
 import productListReducer from "../features/product_data/productListSlice";
 import productCategoriesReducer from "../features/product_data/productCategoriesSlice";
@@ -12,7 +11,6 @@ const persistentStore = localStorage.getItem("scandiwebAssignmentSessionData")
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     cart: cartReducer,
     productList: productListReducer,
     productCategories: productCategoriesReducer,
